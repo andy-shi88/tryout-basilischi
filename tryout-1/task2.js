2041a22d85d14d61107cc2cee739689e8c13a13f
andy-shi88 <shi88.andy@gmail.com>
2017-03-25 12:12:55 +0700

    task 2 requested body instead of title and doc type [problem wait to be solved]

--- tryout-1/task2.js
+++ tryout-1/task2.js
@@ -1,7 +1,12 @@
 "use strict";
+let request = require("request");
 
 if (process.argv.length <= 2) {
   // console.log("Usage: " + __filename + " SOME_PARAM");
   process.exit(-1);
 }
 var url = process.argv[2];
+
+request(url, function(error, response, body) {
+  console.log(body);
+});
