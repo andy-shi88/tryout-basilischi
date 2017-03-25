bac0b91c98e0e4b76af66d4731ebb3c0cd473cf4
andy-shi88 <shi88.andy@gmail.com>
2017-03-25 10:35:19 +0700

    added read file separate by spaces function

--- tryout-1/tryout-1.js
+++ tryout-1/tryout-1.js
@@ -1,9 +1,18 @@
+if (process.argv.length <= 2) {
+  console.log("Usage: " + __filename + " SOME_PARAM");
+  process.exit(-1);
+}
 
-    if (process.argv.length <= 2) {
-        console.log("Usage: " + __filename + " SOME_PARAM");
-        process.exit(-1);
+var file_path = process.argv[2];
+
+//load file_path
+var fs = require('fs');
+fs.readFile('./' + file_path, function(err, data) {
+    if(err) throw err;
+    var array = data.toString().split(" ");
+    for(i in array) {
+        console.log(array[i]);
     }
+});
 
-    var param = process.argv[2];
-
-    console.log('Param: ' + param);
+console.log('Param: ' + file_path);
