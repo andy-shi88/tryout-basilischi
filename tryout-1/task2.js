ffdff1153d9b652ef27b14aa90a922b0b31dd296
andy-shi88 <shi88.andy@gmail.com>
2017-03-28 08:49:11 +0700

    added check isHTML page method

--- tryout-1/task2.js
+++ tryout-1/task2.js
@@ -1,12 +1,25 @@
 "use strict";
+//import lib
 let request = require("request");
-
+//check param
 if (process.argv.length <= 2) {
   // console.log("Usage: " + __filename + " SOME_PARAM");
   process.exit(-1);
 }
+//load param
 var url = process.argv[2];
-
-request(url, function(error, response, body) {
-  console.log(body);
+//functions
+let isHtml = function(contentHeader) {
+  if(contentHeader.includes("text/html")){
+      return true;
+  }else {
+    return false;
+  }
+}
+request(url, function(error, res, body) {
+  // console.log(res.headers);
+  let conHead = "" + res.headers['content-type'];
+  if(isHtml(conHead))
+    console.log("this is an HTML page");
+  // console.log(body);
 });
